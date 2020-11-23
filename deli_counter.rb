13f44abc827d504c5f05bf5081714e423c35c19b
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-23 02:40:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,8 @@ def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
-    line = []
-    katz_deli.each.with_index(1) do |name, index|
-    line << "#{index}. #{name}"
-    final_line = line.join (" ")
-    puts "The line is currently #{final_line}"
-    ##its not looping
-    end
+    line = katz_deli.map.with_index(1) {|name, index| "#{index}. #{name}"}.join(" ")
+    puts "The line is currently: #{line}"
   end
 end
 
@@ -18,4 +13,13 @@ def take_a_number(katz_deli, person)
   katz_deli << person
   line_position = katz_deli.index(person) + 1
   puts "Welcome, #{person}. You are number #{line_position} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
 end
